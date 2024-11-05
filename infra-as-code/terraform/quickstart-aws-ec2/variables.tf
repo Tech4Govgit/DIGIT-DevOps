8d6bce04aa8ed5455f9985ab06db9eeaf1e882ef
PiramalManish <tech4gov@piramalswasthya.org>
2024-11-05 15:26:37 +0530

    Update variables.tf

--- infra-as-code/terraform/quickstart-aws-ec2/variables.tf
+++ infra-as-code/terraform/quickstart-aws-ec2/variables.tf
@@ -19,7 +19,7 @@ variable "tag" {
 ## change ssh public_key with your public ssh key
 
 variable "public_key" {
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCrfbaDFN3FmjUoVUx4YH1eHPruFbWz6JGPfSKTwIqT75xFzU/Q6KCa3Xa6FnEOpcUKXej95pkeUnXywohojF6FrNak5p5xfGmmwC8UA9s5UxsI7flBKVnjsAbcRuxoa/AtOzg4Cizz6zQLl2JZAivZU1PwZjIJo8dcum9bjZYVHwZc3csKJ2nYgpcQrV8AWnfKtLvl5WNfNF0i5bWOieNLKiEc5DtsKYbQ6umrhhCaoGcH0S/Gy6w0PPSnnfl/AWXO7ckFtEXQbdz9Y15zeUFKgUsbklXxmC6D37BkPGu+IjCZSOttPV+PRM0Dnf0jQLvMV0UhEkguD9ALC5xikqNlFyPH5bGetWDxtLbn61tnoOIYG6lXAdk2Oe35yWWt3ZgcccWtYuRwDo0ofBwY9jWOkEcCefDyYg+S7h1VzNsbB9DsFv0vPcaxHcZK8bLdyhnz1+9rXy/flbiS5kE0O97aZ4zm4wAmqiivN2wWhUez18k2Mcs= demo@demo" 
+  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKD50lsy02+k/AbxxS7IslF+VdlnU3YQ9qacPv88C4IY manish_k@Manish" 
   description = "ssh key"
 }
 
